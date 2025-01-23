0b7da2bf2022cdeb9295e09ba32a00e1c9ee05ec
MohJaber93 <mohjbr.dev@gmail.com>
2025-01-23 19:30:04 +0200

    Edit getProjects function

--- lib/actions/map.actions.ts
+++ lib/actions/map.actions.ts
@@ -1,38 +1,46 @@
 "use server";
 
-export const getProjects = async () => {
+import { PaginatedProjects } from "@/types/map.types";
 
+export const getProjects = async () => {
   try {
-    const response = await fetch('http://18.116.28.100/api/v1/public/projects', {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await fetch(
+      "http://18.116.28.100/api/v1/public/projects",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     if (!response.ok) {
       throw new Error(`Failed to fetch projects: ${response.statusText}`);
     }
 
-    const data = await response.json();
-    const { items: projects } = data; 
+    const data: PaginatedProjects = await response.json();
+    const { items: projects } = data;
     return projects;
-
   } catch (error) {
-    console.error("An error occurred while getting projects from the API:", error);
+    console.error(
+      "An error occurred while getting projects from the API:",
+      error
+    );
     throw error;
   }
-}
+};
 
 export const getProjectById = async (projectId: string) => {
-
   try {
-    const response = await fetch(`http://18.116.28.100/api/v1/public/projects/${projectId}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await fetch(
+      `http://18.116.28.100/api/v1/public/projects/${projectId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     if (!response.ok) {
       throw new Error(`Failed to fetch project: ${response.statusText}`);
@@ -40,17 +48,18 @@ export const getProjectById = async (projectId: string) => {
 
     const project = await response.json();
     return project;
-
   } catch (error) {
-    console.error("An error occurred while getting the project from the API:", error);
+    console.error(
+      "An error occurred while getting the project from the API:",
+      error
+    );
     throw error;
   }
-}
+};
 
 export const getUnits = async () => {
-
   try {
-    const response = await fetch('http://18.116.28.100/api/v1/public/units', {
+    const response = await fetch("http://18.116.28.100/api/v1/public/units", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -63,22 +72,26 @@ export const getUnits = async () => {
 
     const units = await response.json();
     return units;
-
   } catch (error) {
-    console.error("An error occurred while getting the units from the API:", error);
+    console.error(
+      "An error occurred while getting the units from the API:",
+      error
+    );
     throw error;
   }
-}
+};
 
 export const getUnitById = async (unitId: string) => {
-
   try {
-    const response = await fetch(`http://18.116.28.100/api/v1/public/units/${unitId}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await fetch(
+      `http://18.116.28.100/api/v1/public/units/${unitId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     if (!response.ok) {
       throw new Error(`Failed to fetch units: ${response.statusText}`);
@@ -86,22 +99,26 @@ export const getUnitById = async (unitId: string) => {
 
     const unit = await response.json();
     return unit;
-
   } catch (error) {
-    console.error("An error occurred while getting the unit from the API:", error);
+    console.error(
+      "An error occurred while getting the unit from the API:",
+      error
+    );
     throw error;
   }
-}
+};
 
 export const getCategories = async () => {
-
   try {
-    const response = await fetch('http://localhost:3000/api/v1/public/unit-category', {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await fetch(
+      "http://localhost:3000/api/v1/public/unit-category",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     if (!response.ok) {
       throw new Error(`Failed to fetch categories: ${response.statusText}`);
@@ -109,35 +126,41 @@ export const getCategories = async () => {
 
     const categories = await response.json();
     return categories;
-
   } catch (error) {
-    console.error("An error occurred while getting the categories from the API:", error);
+    console.error(
+      "An error occurred while getting the categories from the API:",
+      error
+    );
     throw error;
   }
-}
+};
 
 export const getPlaces = async () => {
-
   try {
-    const response = await fetch('http://18.116.28.100/api/v1/public/project-facilities', {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await fetch(
+      "http://18.116.28.100/api/v1/public/project-facilities",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     if (!response.ok) {
       throw new Error(`Failed to fetch categories: ${response.statusText}`);
     }
 
     const data = await response.json();
-    const { items: places } = data; 
-    return places;
-    
+    const { items: places } = data;
     return places;
 
+    return places;
   } catch (error) {
-    console.error("An error occurred while getting the categories from the API:", error);
+    console.error(
+      "An error occurred while getting the categories from the API:",
+      error
+    );
     throw error;
   }
-}
+};
